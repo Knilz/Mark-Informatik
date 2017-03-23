904a1ac362b93fcf830bd5f79d7aa2c66fe1200d
Knilz <mark-bart@web.de>
2017-03-23 12:38:23 +0100

    Update shift.js

--- shift.js
+++ shift.js
@@ -5,3 +5,10 @@ verschobenerText+= String.fromCharCode(text.charCodeAt(i)+anzahlVerschiebungen);
 }
 return verschobenerText;
 }
+function findeParagraph(nummer,sprache){
+	return document.getElementsByClassName(sprache)[nummer-1];
+}
+function shiftText(nummer,sprache,verschiebung){
+	paragraph = findeParagraph(nummer,sprache);
+	paragraph.innerText = shift(paragraph.innerText,verschiebung);
+}
