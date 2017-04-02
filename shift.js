3fd2eee7234716605a9cafa058f6774db5745162
Knilz <mark-bart@web.de>
2017-04-03 00:39:14 +0200

    Update shift.js

--- shift.js
+++ shift.js
@@ -2,31 +2,37 @@ function shift(text, anzahlVerschiebungen){
 var verschobenerText = "";
 for(var i = 0;i<text.length;i++){
 	var charcode = text.charCodeAt(i);
-	if(charcode>64 && charcode<91) 
-		verschobenerText+= String.fromCharCode(((charcode+anzahlVerschiebungen)%92)+65);
-	else if(charcode>96 && charcode<123)
-		verschobenerText+= String.fromCharCode(((charcode+anzahlVerschiebungen)%123)+97);
-	else 
-		verschobenerText+= String.fromCharCode(charcode);
+	verschobenerText += String.fromCharcode(shiftCc(charcode,anzahlVerschiebungen)); 
 }
 return verschobenerText;
 }
-function findeParagraph(nummer,sprache){
-	return document.getElementsByClassName(sprache)[nummer-1];
-}
 function shiftText(nummer,sprache,verschiebung){
-	paragraph = findeParagraph(nummer,sprache);
+	paragraph = document.getElementsByClassName(sprache)[nummer-1];
 	paragraph.innerText = shift(paragraph.innerText,verschiebung);
 }
-function paragraphListe(classname){
-	return document.getElementsByClassName(classname);	
-}
 function cShift(classname,verschiebung){
-	var paragraphliste = paragraphListe(classname);
+	var paragraphliste = document.getElementsByClassName(classname);
 	for(var i = 0; i< paragraphliste.length;i++){
-		paragraphliste[i].innerText = shift(paragraphliste[i].innerText,verschiebung);	
+		var iT = paragraphliste[i].innerText;
+		paragraphliste[i].innerText = shift(iT,verschiebung);	
 	}
 }
+function shiftCc(charcode,anzahlVerschiebungen){
+	if(!((charcode>64 && charcode<91)||(charcode>96 && charcode<123)))
+		return charcode;
+	var cc51 = inASCIIBereich51(charcode);
+	cc51 = cc51+anzahlVerschiebungen % 52;
+	return inASCIIBereich(cc51);
+}
 function inASCIIBereich51(charcode){
-	if(charcode		
+	if(charcode<91)
+		return charcode-65;
+	else
+		return charcode-71;
+}
+function inASCIIBereich(charcode){
+	if(charcode<26)
+		return charcode+65;
+	else
+		return charcode+71;
 }
